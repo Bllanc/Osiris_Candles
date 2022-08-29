1a5e6cbd1363b23e5b9dca4f8e0d33c0a36b7bd3
Allan <allan.averlan@laposte.net>
2022-08-29 16:01:22 +0200

    modif

--- bdd/clear_panier.php
+++ bdd/clear_panier.php
@@ -1,14 +1,6 @@
 <?php
 
 session_start();
+
+unset($_SESSION["nb_panier"]);
 header("Location: ../controleur/panier.php");
-
-
-
-unlink('plaques_utilisateurs/' . $_SESSION['email'] . '/' .  strtok($_SESSION['panier_titre_' . $_POST['delete']], " ") . '.php');
-
-unset($_SESSION['panier_id_' . $_POST['delete']]);
-unset($_SESSION['panier_quantite_' . $_POST['delete']]);
-unset($_SESSION['panier_url_' . $_POST['delete']]);
-unset($_SESSION['panier_titre_' . $_POST['delete']]);
-unset($_SESSION['panier_prix_' . $_POST['delete']]);
